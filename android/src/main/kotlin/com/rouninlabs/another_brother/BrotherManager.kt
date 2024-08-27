0841a0080d4dd4ad9949b3e57c103d2efcd5b5b0
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2024-08-27 10:37:54 -0300

    fix: resolve deadlock exception

--- android/src/main/kotlin/com/rouninlabs/another_brother/BrotherManager.kt
+++ android/src/main/kotlin/com/rouninlabs/another_brother/BrotherManager.kt
@@ -1,28 +1,25 @@
 package com.rouninlabs.another_brother
 
 import android.app.PendingIntent
-import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.hardware.usb.UsbDevice
 import android.hardware.usb.UsbManager
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.annotation.WorkerThread
 import com.brother.ptouch.sdk.Printer
 import com.rouninlabs.another_brother.method.typeb.ITbPrinterAdapter
 import com.rouninlabs.another_brother.receiver.UsbPermissionsReceiver
-import java.util.concurrent.ArrayBlockingQueue
-import java.util.concurrent.BlockingQueue
+import java.util.concurrent.CompletableFuture
 
 /**
  * Tracker active printers to support the open/print/close approach.
  */
 object BrotherManager {
-
     val mActivePrinters: MutableMap<String, Printer> = hashMapOf()
     val mActiveTypeBPrinters: MutableMap<String, ITbPrinterAdapter> = hashMapOf()
-
-    val mUsbPermissionRequests: MutableMap<Int, BlockingQueue<Boolean>> = hashMapOf()
+    val mUsbPermissionRequests = mutableMapOf<Int, CompletableFuture<Boolean>>()
 
     fun getPrinter(printerId: String): Printer? {
         return mActivePrinters[printerId]
@@ -51,43 +48,40 @@ object BrotherManager {
     /**
      * Makes a permission request to get access to the usb device
      */
+    @RequiresApi(Build.VERSION_CODES.N)
     @WorkerThread
     fun requestUsbPermission(
-        context: Context,
-        usbManager: UsbManager,
-        usbDevice: UsbDevice
-    ): Boolean {//:BlockingQueue<Boolean> {
+            context: Context,
+            usbManager: UsbManager,
+            usbDevice: UsbDevice
+    ): Boolean {
         val requestId = usbDevice.deviceId
         if (mUsbPermissionRequests.containsKey(requestId)) {
-            return mUsbPermissionRequests[requestId]!!.take()
+            return mUsbPermissionRequests[requestId]!!.get()
         }
 
-        val completableFuture = ArrayBlockingQueue<Boolean>(1)
-        mUsbPermissionRequests.put(requestId, completableFuture)
+        val completableFuture = CompletableFuture<Boolean>()
+        mUsbPermissionRequests[requestId] = completableFuture
         val intent = Intent(context, UsbPermissionsReceiver::class.java)
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             usbManager.requestPermission(
-                usbDevice,
-                PendingIntent.getBroadcast(context, 1234, intent, PendingIntent.FLAG_IMMUTABLE)
+                    usbDevice,
+                    PendingIntent.getBroadcast(context, 1234, intent, PendingIntent.FLAG_IMMUTABLE)
             )
         } else {
             usbManager.requestPermission(
-                usbDevice,
-                PendingIntent.getBroadcast(context, 1234, intent, 0)
+                    usbDevice,
+                    PendingIntent.getBroadcast(context, 1234, intent, 0)
             )
         }
-        return completableFuture.take()
+        return completableFuture.get()
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     fun completePermissionRequest(usbDevice: UsbDevice, granted: Boolean) {
         val requestId = usbDevice.deviceId
-        if (!mUsbPermissionRequests.containsKey(requestId)) {
-            return
-        }
-
-        mUsbPermissionRequests[requestId]?.put(granted)
+        mUsbPermissionRequests[requestId]?.complete(granted)
         mUsbPermissionRequests.remove(requestId)
-
     }
 }
